4c24a9746da8e4a995acad384e632922bf90118e
Seyda Koclar <seydakoclar@gmail.com>
2022-01-16 00:18:37 +0100

    first two assignments

--- fundamentals.js
+++ fundamentals.js
@@ -29,3 +29,51 @@ console.log(typeof (isIsland));
 console.log(typeof (population));
 console.log(typeof (country));
 console.log(typeof (language));
+
+/*
+ASSIGNMENT 3
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
+*/
+
+language = "Turkish";
+const country = "Turkey";
+const continent = "Asia";
+const isIsland = false;
+
+country = "Another";
+
+/*
+ASSIGNMENT 4
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+*/
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+let description = country + 'is in ' + continent + ', and its ' + population + ' million people speak' + language;
+
+console.log(description);
+
+/*
+ASSIGNMENT 5
+1. Recreate the 'description' variable from the last assignment, this time
+using the template literal syntax
+*/
+
+let newDescription = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
