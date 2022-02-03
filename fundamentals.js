0595b15d8267fbe243d65ec940ec5aea88c98e19
Seyda Koclar <seydakoclar@gmail.com>
2022-02-03 16:23:31 +0100

    added assignment 9

--- fundamentals.js
+++ fundamentals.js
@@ -145,7 +145,7 @@ when you input 1
 situation
 */
 
-let numNeighbours = prompt("How many neighbour countries does your country have?");
+let numNeighbours //= prompt("How many neighbour countries does your country have?");
 
 //this works as we expected however if we use === in here it will not work when we type 1 because it is a string type variable and strict equality looks exact equality.
 if (numNeighbours == 1) {
@@ -175,3 +175,27 @@ else {
 }
 
 */
+
+/*
+ASSIGNMENT 9
+1. Comment out the previous code so the prompt doesn't get in the way
+2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+country that speaks english, has less than 50 million people and is not an
+island.
+3. Write an if statement to help Sarah figure out if your country is right for her.
+You will need to write a condition that accounts for all of Sarah's criteria. Take
+your time with this, and check part of the solution if necessary.
+4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+not, log 'Portugal does not meet your criteria :('
+5. Probably your country does not meet all the criteria. So go back and temporarily
+change some variables in order to make the condition true (unless you live in
+Canada :D)
+*/
+
+if (language === "English" && population < 50 && isIsland === false) {
+    console.log(`You should live in ${country} :)`);
+}
+else {
+    console.log(`${country} does not meet your criteria :(`);
+}
+
