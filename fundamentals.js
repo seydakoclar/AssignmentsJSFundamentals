a349e5effb775d670bfced77a3307903d8db11cf
Seyda Koclar <seydakoclar@gmail.com>
2022-02-03 12:29:41 +0100

    added assignment  8

--- fundamentals.js
+++ fundamentals.js
@@ -119,7 +119,59 @@ GUESSES
 */
 
 console.log('9' - '5');                     //corret guess
-console.log('19' - '13' + '17');            //correct gues
-console.log('19' - '13' + 17);              //correct gues
-console.log('123' < 57);                    //correct gues
-console.log(5 + 6 + '4' + 9 - 4 - 2);       //correct gues
+console.log('19' - '13' + '17');            //correct guess
+console.log('19' - '13' + 17);              //correct guess
+console.log('123' < 57);                    //correct guess
+console.log(5 + 6 + '4' + 9 - 4 - 2);       //correct guess
+
+/*
+ASSIGNMENT 8
+1. Declare a variable 'numNeighbours' based on a prompt input like this:
+prompt('How many neighbour countries does your country
+have?');
+2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality
+== for now)
+3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours'
+is greater than 1
+4. Use an else block to log 'No borders' (this block will be executed when
+'numNeighbours' is 0 or any other value)
+5. Test the code with different values of 'numNeighbours', including 1 and 0.
+6. Change == to ===, and test the code again, with the same values of
+'numNeighbours'. Notice what happens when there is exactly 1 border! Why
+is this happening?
+7. Finally, convert 'numNeighbours' to a number, and watch what happens now
+when you input 1
+8. Reflect on why we should use the === operator and type conversion in this
+situation
+*/
+
+let numNeighbours = prompt("How many neighbour countries does your country have?");
+
+//this works as we expected however if we use === in here it will not work when we type 1 because it is a string type variable and strict equality looks exact equality.
+if (numNeighbours == 1) {
+    console.log("Only 1 border!")
+}
+else if (numNeighbours > 1) {
+    console.log("More than 1 border");
+}
+else {
+    console.log("No borders");
+}
+
+/*
+Other option that can be used and preferred.
+
+
+let numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+
+if (numNeighbours === 1) {
+    console.log("Only 1 border!")
+}
+else if (numNeighbours > 1) {
+    console.log("More than 1 border");
+}
+else {
+    console.log("No borders");
+}
+
+*/
